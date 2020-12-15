263d196e2dbff56930d7bb9f3ab1144da035bf6c
Marcus Toftås <toftasm@student.chalmers.se>
2020-12-15 21:08:26 +0100

    Updated Background Tests

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from Plot import Plot
 import numpy as np
+import timeit
 
 class Main():
 
@@ -8,27 +9,32 @@ class Main():
 
         self.root = tk.Tk()
         self.root.withdraw()
-        self.plot_canvas = Plot(1000, 600, 'wtf')
+        self.plot_canvas = Plot(1000, 600, 'Testing Utils')
 
-        a = np.random.uniform(0,1,100)
+        a = np.random.uniform(0,90,100)
         b = np.random.uniform(0,90,100)
-        c = np.random.uniform(0,1,100)
+        c = np.random.uniform(0,90,100)
         d = np.random.uniform(0,90,100)
 
-        self.plot_canvas.set_legend(['a', 'b'], [1,2], 'NE')
-        self.plot_canvas.set_title('Tada')
+        self.plot_canvas.set_legend(['a', 'b'], [1,2], 'SW')
+        self.plot_canvas.set_title('Schkii Schkii')
         self.plot_canvas.set_labels('xxx','yyy')
-        self.plot_canvas.graph(b,a, 'testData', 'show')
-        self.plot_canvas.graph(d,c, 'testData2')
-        self.plot_canvas.set_x_axis(0,110)
-        self.plot_canvas.set_y_axis(-0.1,1.2)
-        self.plot_canvas.colorbar(['red','green','blue'],[0,0.5,1], 3, 'cb')
+        self.plot_canvas.graph(b,a, 'testData', 'show', 'scatter')
+        # self.plot_canvas.graph(d,c, 'testData2')
+        # self.plot_canvas.set_x_axis(0,110)
+        # self.plot_canvas.set_y_axis(-0.1,100.2)
+        
         self.plot_canvas.add_text([3, 0.05], 'tatat', 'tex1')
-        self.plot_canvas.update_text('wopp wopp', 'tex1')
+        self.plot_canvas.update_text('Hahlelelha', 'tex1')
+        self.plot_canvas.set_axis_scale_type('digit')
 
-        # self.plot_canvas.clear_dataset('testData')
+        self.test_code()
+        # print(timeit.timeit(self.test_code, number=10000))
 
         self.mainloop()
+
+    def test_code(self):
+        self.plot_canvas.add_colorbar('yellow','green',[0,0.5,1])
 
     def mainloop(self):
         self.root.mainloop()
