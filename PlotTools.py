c5032194314ba501a47d95303defd626fb9a2f55
Marcus Toftås <toftasm@student.chalmers.se>
2021-02-04 16:59:30 +0100

    Class update for axis numbers + grid

--- PlotTools.py
+++ PlotTools.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Grid:
 
-    def __init__(self, direction, canvas, steps=None, style='lin', line_visibility=True):
+    def __init__(self, direction, canvas, color=None, steps=None, style='lin', line_visibility=True):
         """ steps = number of grid- lines/numbers
             line_visibility = if gridlines active """
 
@@ -14,10 +14,12 @@ class Grid:
         if steps == None:
             if style == 'lin': self.number_of_steps = 10
             elif style == 'log': self.number_of_steps = 3
+        else: self.number_of_steps = steps
+        self.col = color
 
         # Actual Lines
-        self.pos = np.zeros(self.number_of_steps)
-        self.lines = []
+        self.pos = np.zeros(self.number_of_steps + 1)
+        self.drawn_grid = []
 
         self.update_pos()
 
@@ -38,7 +40,7 @@ class Grid:
 
     def set_number_of_steps(self, steps):
         self.number_of_steps = int(steps)
-        self.pos = np.zeros(self.number_of_steps)
+        self.pos = np.zeros(self.number_of_steps + 1)
         self.update_pos()
     def get_number_of_steps(self):
         return self.number_of_steps
@@ -52,19 +54,18 @@ class Grid:
 
         if self.direction == 'x':
             step = int(self.canvas.winfo_width() / self.number_of_steps)
-            for i in range(self.number_of_steps):
+            for i in range(self.number_of_steps + 1):
                 self.pos[i] =  i * step
         else: 
             offset = self.canvas.winfo_height()
             step = int(self.canvas.winfo_height() / self.number_of_steps)
-            for i in range(self.number_of_steps):
+            for i in range(self.number_of_steps + 1):
                 self.pos[i] =  offset - i * step
-        print('a %s' %self.pos)
         self.redraw()
 
     def remove(self):
-        for item in self.lines: self.canvas.delete(item)
-        self.lines.clear() 
+        for item in self.drawn_grid: self.canvas.delete(item)
+        self.drawn_grid.clear() 
 
     def redraw(self):
         
@@ -82,14 +83,14 @@ class Grid:
         end = 0 if self.line_visibility else self.canvas.winfo_height() - 5
         for val in self.pos:
             pos = [val, self.canvas.winfo_height(), val, end]
-            self.lines.append(self.canvas.create_line(pos, fill="gray"))
+            self.drawn_grid.append(self.canvas.create_line(pos, fill="gray"))
 
     def __lin_y(self):
         
         end = self.canvas.winfo_width() if self.line_visibility else 5
         for val in self.pos:
             pos = [0, val, end, val]
-            self.lines.append(self.canvas.create_line(pos, fill="gray"))
+            self.drawn_grid.append(self.canvas.create_line(pos, fill="gray"))
 
     def __log_x(self):
 
@@ -105,9 +106,9 @@ class Grid:
             for j in range(9):
                 pixel = (1 + k * (i + np.log10(1 + j)))/scale_factor * width
                 pos = [pixel, height, pixel, end]
-                self.lines.append(self.canvas.create_line(pos, fill="gray"))
+                self.drawn_grid.append(self.canvas.create_line(pos, fill="gray"))
 
-    def __log_y(self):        
+    def __log_y(self):
 
         width = self.canvas.winfo_width()
         height = self.canvas.winfo_height()
@@ -121,59 +122,74 @@ class Grid:
             for j in range(9):
                 pixel = height - (1 + k * (i + np.log10(1 + j)))/scale_factor * height
                 pos = [0, pixel, end, pixel]
-                self.lines.append(self.canvas.create_line(pos, fill="gray"))
+                self.drawn_grid.append(self.canvas.create_line(pos, fill="gray"))
 
 class Axis_Numbers:
 
-    def __init__(self, direction, canvas, position):
+    def __init__(self, direction, canvas, pos, color):
         """ direction = x/y, canvas = Plot.canvas """
 
         self.canvas = canvas
         self.direction = direction
         self.axis_values = []
-        self.drawn_items = []
+        self.drawn_numbers = []
+        self.col = color
+        self.lower = 0
+        self.upper = 0
 
-        self.position = position
+        self.pos = pos
 
     def set_axis_values(self, values):
-        length = len(self.axis_values)
         self.axis_values = values
-        self.redraw(length=length)
+        self.update()
     def get_axis_values(self):
         return self.axis_values
+    def set_lower_axis(self, lower):
+        self.lower = float(lower)
     def get_lower_axis(self):
-        return self.axis_values[0]
+        return self.lower
+    def set_upper_axis(self, upper):
+        self.upper = float(upper)
     def get_upper_axis(self):
-        return self.axis_values[-1]
+        return self.upper
 
-    def set_position(self, position):
-        self.position = position
+    def set_pos(self, pos, update=False):
+        self.pos = pos
+        if update == True: self.redraw()
 
     def update_numbers(self, plot_width, canvas_boundary, font_size):
         self.plot_width = plot_width
         self.canvas_boundary = canvas_boundary
         self.font_size = font_size
+        self.redraw()
 
-    def draw(self):
+    def update(self):
+        
+        if len(self.drawn_numbers) != len(self.axis_values):
+            self.redraw()
+            return
+        
+        for i in range(len(self.axis_values)):
+            self.canvas.itemconfig(self.drawn_numbers[i], text = self.axis_values[i])
+
+    def remove(self):
+        for item in self.drawn_numbers: self.canvas.delete(item)
+        self.drawn_numbers.clear()
+
+    def redraw(self):
+
+        self.remove()
 
         if self.direction == 'x':
-            step_size = self.plot_width/len(self.axis_values)
-            for i in range(len(self.axis_values)-1):
-                pos = [self.position[i], 
-                       self.canvas.winfo_height() - 3*self.font_size]
-                tex = '{:.2f}'.format(self.axis_values[i])
-                self.drawn_items.append(self.canvas.create_text(pos, 
-                                            anchor = N, 
-                                            text = tex))
-
-    def redraw(self, length=None):
-        
-        if length != len(self.axis_values):
-            for item in self.drawn_items: self.canvas.delete(item)
-            self.draw()
-            return
-
-        for i in range(len(self.axis_values)):
-            self.canvas.itemconfig(self.axis_values[i], text=values[i])
-            
+            for i in range(len(self.axis_values)):
+                pos = [self.pos[i] + self.canvas_boundary, 
+                       self.canvas.winfo_height() - self.canvas_boundary + self.font_size]
+                self.drawn_numbers.append(self.canvas.create_text(pos, anchor = N, 
+                                        text = self.axis_values[i], fill=self.col))
+        else:
+            for i in range(len(self.axis_values)):
+                pos = [self.canvas_boundary - self.font_size/2, 
+                       self.pos[i] + self.canvas_boundary]
+                self.drawn_numbers.append(self.canvas.create_text(pos, anchor = E, 
+                                        text = self.axis_values[i], fill=self.col))
             
